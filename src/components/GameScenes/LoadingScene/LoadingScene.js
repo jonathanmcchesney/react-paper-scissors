74c64481c1accf0a462d2f0b9e67e9a7a9c5d174
Erin Hughes <Erin.Hughes@ibm.com>
2020-10-02 18:27:15 +0100

    Added loading scene before results scene
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>

---
+++ src/components/GameScenes/LoadingScene/LoadingScene.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './LoadingScene.css';
+
+const optionStrings = ['ROCK!', 'PAPER!', 'SCISSORS!'];
+
+class LoadingScene extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            optionsIndex: 0
+        }
+    }
+    
+
+    componentDidMount() {
+        this.timeout = setInterval(() => {
+            let newIndex = (this.state.optionsIndex) + 1;
+            if (newIndex > optionStrings.length -1) {
+                clearInterval(this.timeout);
+                this.props.flipShowLoadingScene();
+            }
+            this.setState({
+                optionsIndex: newIndex
+            });
+        }, 500);
+    }
+
+    render() {
+        return (
+            <>
+                <div className='loading-titles-container'>
+                    <h1>{optionStrings[this.state.optionsIndex]}</h1>
+                </div>
+                <div className='loading-emojis-container'>
+                    <span role='img' aria-label='left-fist' className='fist-emoji left-fist'>🤜</span>
+                    <span role='img' aria-label='right-fist' className='fist-emoji right-fist'>🤛</span>
+                </div>
+            </>
+        );
+    }
+}
+
+export default LoadingScene;
